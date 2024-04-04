a10f44fd74984dc53ba9b640f3af9b03abc3014f
DavideFocalors <150442316+DavideFocalors79@users.noreply.github.com>
2024-04-04 22:07:23 +0200

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,16 @@
+const pi=3.14;
+const r1=25,r2=15;
+
+console.log("il primo raggio e:",r1);
+console.log("il secondo raggio e:",r2);
+
+let a1=(r1**2)*pi,a2=(r2**2)*pi;
+
+
+console.log("la prima area e:",a1);
+console.log("la seconda area e:",a2);
+
+let bool=a1<a2;
+
+console.log(bool);
+
